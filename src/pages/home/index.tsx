3b0049af49208a68dbb9b934cb0b26252f469135
Mato <Kittawat@icloud.com>
2023-11-23 19:45:44 +0800

    3

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { pokemonDetailServices, pokemonListServices } from "@/services";
+import SearchForm from "@/components/SearchForm";
 
 const HomePage = () => {
   const callData = async () => {
@@ -11,7 +12,18 @@ const HomePage = () => {
     callData();
   }, []);
 
-  return <div>HomePage</div>;
+  return (
+    <div className=" w-[90%] m-[auto] max-w-[1100px]">
+      <div className="flex justify-center">
+        <img
+          src="/images/logo.webp"
+          className="max-h-[80px] mt-[20px]"
+          alt=""
+        />
+      </div>
+      <SearchForm />
+    </div>
+  );
 };
 
 export default HomePage;
