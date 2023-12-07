e7e715ff30ebb34a122493a3cc47375eca3c66b9
Mato <Kittawat@icloud.com>
2023-12-07 22:54:32 +0800

    done

--- src/components/SearchForm/SearchForm.hook.ts
+++ src/components/SearchForm/SearchForm.hook.ts
@@ -3,6 +3,7 @@ import { pokemonDetailServices, pokemonListServices } from "@/services";
 import { usePokemonListStore } from "@/store/pokemonList";
 import { useForm } from "react-hook-form";
 import { generationList } from "@/utils/optionList";
+import { IPokemonDetailResponse } from "@/interface/pokemonDetail";
 
 const useSearchForm = () => {
   const {
@@ -48,6 +49,7 @@ const useSearchForm = () => {
           });
       }
       setFetchPokemonList({ data: pokeList, loading: false, error: null });
+      const data = filterPokemon(pokeList, keyword, type, sort);
       setPokemonList({ data: pokeList, loading: false, error: null });
     } else {
       setFetchPokemonList({
@@ -58,20 +60,51 @@ const useSearchForm = () => {
     }
   };
 
-  useEffect(() => {
-    console.log("generation", generation);
+  const filterPokemon = (
+    pokeList: IPokemonDetailResponse[],
+    keyword: string,
+    type: string,
+    sort: "id" | "name"
+  ) => {
+    const keywordFilter = pokeList.filter((item) =>
+      item.name.toLowerCase().includes(keyword?.toLowerCase())
+    );
 
+    const typeFilter =
+      type !== "all types"
+        ? keywordFilter.filter((item) =>
+            item.types.find((f) =>
+              f.type.name.toLowerCase().includes(type.toLowerCase())
+            )
+          )
+        : keywordFilter;
+
+    return sortBy(typeFilter, sort);
+  };
+
+  const sortBy = (data: IPokemonDetailResponse[], type: "id|name") => {
+    switch (type) {
+      case "id":
+        return data.sort((a, b) => a.id - b.id);
+      case "name":
+        return data.sort((a, b) =>
+          a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+        );
+      default:
+        return data.sort((a, b) => a.id - b.id);
+    }
+  };
+
+  useEffect(() => {
     if (generation !== undefined) {
       callData(generationList[generation]);
     }
   }, [generation]);
 
   useEffect(() => {
-    const data = fetchPokemon.data.filter((item) =>
-      item.name.toLowerCase().includes(keyword?.toLowerCase())
-    );
+    const data = filterPokemon(fetchPokemon.data, keyword, type, sort);
     setPokemonList({ data: data, loading: false, error: null });
-  }, [keyword]);
+  }, [keyword, type, sort]);
 
   return {
     fieldKeyword: register("keyword"),
